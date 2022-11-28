218614cfcf6fedf8ba1ea1feeaf2673101dfa203
Meshack <meshkipla@gmail.com>
2022-11-28 15:50:38 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 
 // Write your code here
+let num1=2;
+let num2=31;
+let num3=16;
+let num4=12;
+
+let multiply=num1*num2;
+let random=Math.floor(Math.random());
+let mod=num3%num4;
+let max=Math.max(10, 13, 2, 20, 12, 17,12)
